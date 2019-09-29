4a52d654e5eb58daba8c7457d1302a7e8c856965
dev011 <aneeh.ahuja@gmail.com>
2019-09-29 22:12:13 +0530

    feat: players alternate placing X’s and O’s until it's a triplet or filled board
    Includes:
    
    1. Unit Test suite/cases for the scenarios. Changes made in Board.test.js file.
    2. Feature Implementation of the scenarios. Changes made in Board.js file.
    
    Covers the following scenarios:
    Players alternate placing X’s and O’s on the board until either:
        - One player has three in a row, horizontally, vertically or diagonally
        - All nine squares are filled.

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -7,6 +7,10 @@ class Board extends Component {
   XPLAYER_TURN = 'Player X\'s Turn!';
   OPLAYER_TURN = 'Player O\'s Turn!';
 
+  statusMessages = {
+                  "turn": { "X": this.XPLAYER_TURN, "O": this.OPLAYER_TURN}
+                  }
+
   constructor () {
     super()
 
@@ -23,6 +27,7 @@ class Board extends Component {
   updateBoard(squareIndex) {
     if (this.isMoveAllowed(squareIndex)) {
       this.setSquareItem(squareIndex);
+      this.checkGameStatus();
     }
   }
 
@@ -40,6 +45,99 @@ class Board extends Component {
     this.setState({
       board : updatedBoard
     })
+  }
+
+  checkGameStatus() {
+    if (this.isTriplet()) {
+      this.setState({ isGameOver : true})
+    }
+    else if (this.isBoardFull()) {
+      this.setState({ isGameOver : true})
+    }
+    else {
+      this.nextMove();
+    }
+  }
+
+  isTriplet() {
+    let tempBoard = this.tempBoard(this.state.board);
+    return (this.isHorizontalTriplet(tempBoard) || this.isVerticalTriplet(tempBoard) || this.isDiagonalTriplet(tempBoard));
+  }
+
+  isBoardFull() {
+    for (const squareItem of this.state.board){
+      if (squareItem === this.state.squareEnum.EMPTY){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  nextMove() {
+    this.togglePlayer();
+  }
+
+  isHorizontalTriplet(tempBoard) {
+    for (const colArray of tempBoard) {
+      if (this.isEqual(colArray[0], colArray[1], colArray[2])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isVerticalTriplet(tempBoard) {
+    let rowIndex = 0;
+    for (let colIndex = 0; colIndex < tempBoard[rowIndex].length; colIndex++) {
+      if (this.isEqual(tempBoard[0][colIndex], tempBoard[1][colIndex], tempBoard[2][colIndex])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isDiagonalTriplet(tempBoard) {
+    return (this.isLeftDiagonalTriplet(tempBoard) || this.isRightDiagonalTriplet(tempBoard));
+  }
+
+  isLeftDiagonalTriplet(tempBoard) {
+    return this.isEqual(tempBoard[0][0], tempBoard[1][1], tempBoard[2][2]);
+  }
+
+  isRightDiagonalTriplet(tempBoard) {
+    return this.isEqual(tempBoard[0][2], tempBoard[1][1], tempBoard[2][0]);
+  }
+
+  isEqual(squareItem1, squareItem2, squareItem3) {
+    return (squareItem1!=='' && squareItem1 === squareItem2 && squareItem2 === squareItem3);
+  }
+
+  tempBoard(board) {
+    return ([board.slice(0,3), board.slice(3,6), board.slice(6,9)]);
+  }
+
+  togglePlayer() {
+    let updatedCurrentPlayer = this.state.currentPlayer === this.state.squareEnum.xPlayer ? this.state.squareEnum.oPlayer : this.state.squareEnum.xPlayer;
+    this.setState({
+      currentPlayer : updatedCurrentPlayer
+    }) 
+    this.setGameStatus(false, this.getStatusMessage('turn', updatedCurrentPlayer));
+  }
+
+  setGameStatus(isGameOver, gameStatusMessage) {
+    this.setState({
+      isGameOver : isGameOver,
+      gameStatusMessage : gameStatusMessage
+    })
+  }
+
+  getStatusMessage(status, player) {
+    const {turn} = this.statusMessages;
+    switch(player.concat(status)) {
+      case 'Xturn': return turn.X;
+      case 'Oturn': return turn.O;
+      default: return this.state.gameStatusMessage;
+    }
   }
 
   render() {
--- src/components/board/Board.test.js
+++ src/components/board/Board.test.js
@@ -49,3 +49,106 @@ describe('Players cannot play on a played position', function() {
      expect(boardInstance.isMoveAllowed()).toBeFalsy();
    });
 });
+
+describe('Players alternate placing X’s and O’s on the board until a game is over(win or draw)', function(){
+   const togglePlayerSpy = jest.spyOn(boardInstance, 'togglePlayer');
+   let tempBoard = [];
+
+   it('should toggle Players on every next move', function() {
+      boardInstance.nextMove();
+      expect(togglePlayerSpy).toHaveBeenCalled();
+   });
+
+   it('should allow Player `X` to play next if Player `O` played last', function() {
+      squareIndex = 2;
+      boardInstance.state.currentPlayer = squareEnum.oPlayer;
+
+      boardInstance.state.board =  [
+            squareEnum.xPlayer, squareEnum.EMPTY, squareEnum.EMPTY,
+            squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY,
+            squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY
+      ];
+
+      boardInstance.updateBoard(squareIndex);
+      expect(togglePlayerSpy).toHaveBeenCalled();
+      expect(boardInstance.state.currentPlayer).toBe(squareEnum.xPlayer);
+      expect(boardInstance.state.isGameOver).toBeFalsy();
+      expect(boardInstance.state.gameStatusMessage).toBe(boardInstance.getStatusMessage('turn', squareEnum.xPlayer));
+      });
+
+   it('should allow Player `O` to play next if Player `X` played last', function() {
+      squareIndex = 3;
+      boardInstance.state.currentPlayer = squareEnum.xPlayer;
+ 
+      boardInstance.state.board =  [
+          squareEnum.xPlayer, squareEnum.EMPTY, squareEnum.oPlayer,
+          squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY,
+          squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY
+      ];
+ 
+      boardInstance.updateBoard(squareIndex);
+      expect(togglePlayerSpy).toHaveBeenCalled();
+      expect(boardInstance.state.currentPlayer).toBe(squareEnum.oPlayer);
+      expect(boardInstance.state.isGameOver).toBeFalsy();
+      expect(boardInstance.state.gameStatusMessage).toBe(boardInstance.getStatusMessage('turn', squareEnum.oPlayer));
+    });
+
+   it('should continue game if no player has three in a row horizontally', function() {
+      squareIndex = 8;
+      boardInstance.state.currentPlayer = squareEnum.oPlayer;
+      boardInstance.state.board =  [
+         squareEnum.xPlayer, squareEnum.EMPTY, squareEnum.xPlayer,
+         squareEnum.oPlayer, squareEnum.oPlayer, squareEnum.EMPTY,
+         squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.EMPTY
+      ];
+      tempBoard = boardInstance.tempBoard(boardInstance.state.board);
+
+      boardInstance.updateBoard(squareIndex);
+      expect(boardInstance.isHorizontalTriplet(tempBoard)).toBeFalsy();
+      expect(boardInstance.state.isGameOver).toBeFalsy();
+   });
+
+   it('should continue game if no player has three in a row vertically', function() {
+      squareIndex = 7;
+      boardInstance.state.currentPlayer = squareEnum.oPlayer;
+      boardInstance.state.board =  [
+         squareEnum.xPlayer, squareEnum.EMPTY, squareEnum.xPlayer,
+         squareEnum.oPlayer, squareEnum.oPlayer, squareEnum.EMPTY,
+         squareEnum.EMPTY, squareEnum.EMPTY, squareEnum.xPlayer
+        ];
+      tempBoard = boardInstance.tempBoard(boardInstance.state.board);
+  
+      boardInstance.updateBoard(squareIndex);
+      expect(boardInstance.isVerticalTriplet(tempBoard)).toBeFalsy();
+      expect(boardInstance.state.isGameOver).toBeFalsy();
+   });
+
+   it('should continue game if no player has three in a row diagonally', function() {
+      squareIndex = 6;
+      boardInstance.state.currentPlayer = squareEnum.oPlayer;
+      boardInstance.state.board =  [
+         squareEnum.xPlayer, squareEnum.EMPTY, squareEnum.xPlayer,
+         squareEnum.oPlayer, squareEnum.oPlayer, squareEnum.EMPTY,
+         squareEnum.EMPTY, squareEnum.oPlayer, squareEnum.xPlayer
+        ];
+      tempBoard = boardInstance.tempBoard(boardInstance.state.board);
+
+      boardInstance.updateBoard(squareIndex);
+      expect(boardInstance.isDiagonalTriplet(tempBoard)).toBeFalsy();
+      expect(boardInstance.state.isGameOver).toBeFalsy();
+   });
+
+   it('should continue game if all 9 squares are not filled yet', function() {
+      squareIndex = 7;
+      boardInstance.state.currentPlayer = squareEnum.oPlayer;
+      boardInstance.state.board =  [
+         squareEnum.xPlayer, squareEnum.EMPTY, squareEnum.xPlayer,
+         squareEnum.oPlayer, squareEnum.oPlayer, squareEnum.EMPTY,
+         squareEnum.xPlayer, squareEnum.oPlayer, squareEnum.xPlayer
+        ];
+  
+      boardInstance.updateBoard(squareIndex);
+      expect(boardInstance.isBoardFull()).toBeFalsy();
+      expect(boardInstance.state.isGameOver).toBeFalsy();
+   });
+});
