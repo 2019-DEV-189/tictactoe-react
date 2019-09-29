bda27da4e1ec9515e3065301bdaa8eac4c6490a2
dev011 <aneeh.ahuja@gmail.com>
2019-09-29 21:00:16 +0530

    feat: X always goes first
    Includes:
    
    1. Unit Test suite/cases for the scenario. Changes made in Board.test.js file.
    2. Feature Implementation of the scenario. Changes made in .*js and *.css files.
    3. Added dependencies for classnames library being used in Square Component. Changes made in *.json & Square.js

--- src/components/square/Square.js
+++ src/components/square/Square.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import classNames from 'classnames';
 import './Square.css';
 
 class Square extends Component {
+
+  invokeBoard(props) {
+    props.updateSquare(props.squareIndex);
+  }
+
   render() {
     return (
-    <div className="Square">
+    <div className = {classNames ("square " + this.props.squareIndex, 
+                        { colWall: (this.props.squareIndex !==2 && this.props.squareIndex !==5 && this.props.squareIndex !==8), 
+                          rowWall: (this.props.squareIndex !==6 && this.props.squareIndex !==7 && this.props.squareIndex !==8)
+                        })
+                      } onClick={() => this.invokeBoard(this.props)}>
+      <p className = {classNames ({ xColor : this.props.squareItem === 'X', oColor : this.props.squareItem === 'O'})}>
+        {this.props.squareItem}
+      </p>
     </div>
     );
   }
 }
-
 export default Square;
